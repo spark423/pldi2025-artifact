ae3146b05af63053813191a6ae85c815235de96a
Sehyeok Park <sp2044@golden-cove.cs.rutgers.edu>
2025-03-09 15:48:30 -0400

    Fixing incorrect text in correctness test

--- correctness/rlibm/sinhf.c
+++ correctness/rlibm/sinhf.c
@@ -7,6 +7,6 @@ int main(int argc, char** argv) {
         printf("Usage: %s <log file>\n", argv[0]);
         exit(0);
     }
-    RunTest(argv[1], "Rounding-invariant outputs sinhf");
+    RunTest(argv[1], "Original RLIBM sinhf");
     return 0;
 }
