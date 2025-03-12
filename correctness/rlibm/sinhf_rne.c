25fe0462662fc0fd974e190a9b47b1511622431e
Sehyeok Park <sp2044@rome.cs.rutgers.edu>
2025-03-12 00:39:10 -0400

    Adding correctness tests for original RLIBM functions running under RNE

---
+++ correctness/rlibm/sinhf_rne.c
@@ -0,0 +1,12 @@
+#define __ELEM__ rlibm_sinhf
+#define __MPFR_ELEM__ mpfr_sinh
+#include "LibTestHelperRNE.h"
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Usage: %s <log file>\n", argv[0]);
+        exit(0);
+    }
+    RunTest(argv[1], "Original RLIBM sinhf under RNE");
+    return 0;
+}
