03ccdbe473a77f4e0f99b4ebb7f8eb24c3b2a36c
Sehyeok Park <sp2044@rome.cs.rutgers.edu>
2025-03-13 11:46:04 -0400

    Correcting RLIBM correctness scripts

--- correctness-all/RunSinpiRLIBM.sh
+++ correctness-all/RunSinpiRLIBM.sh
@@ -2,5 +2,5 @@
 touch results/rlibm_sinpif.txt
 touch results/rlibm_sinpif_rne.txt
 echo -e "|  sinpi(x)  |"
-./ourlibm/sinpif results/ourlibm_sinpif.txt
-./ourlibm/sinpif_rz results/ourlibm_sinpif_rz.txt
+./rlibm/sinpif results/rlibm_sinpif.txt
+./rlibm/sinpif_rne results/rlibm_sinpif_rne.txt
--- correctness/RunCosRLIBM.sh
+++ correctness/RunCosRLIBM.sh
@@ -2,5 +2,5 @@
 touch results/rlibm_cosf.txt
 touch results/rlibm_cosf_rne.txt
 echo -e "|  cos(x)  |"
-./core-math/cosf results/core_math_cosf.txt
-./glibc/cos results/glibc_double_cos.txt
+./rlibm/cosf results/rlibm_cosf.txt
+./rlibm/cosf_rne results/rlibm_cosf_rne.txt
