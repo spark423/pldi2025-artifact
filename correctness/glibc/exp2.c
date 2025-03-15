0a6d3afa7784f8bbb076e0fcfcd8bd9c6107ba78
Sehyeok Park <sp2044@sunny-cove.cs.rutgers.edu>
2025-03-15 19:51:39 -0400

    Adding special case for exp2

--- correctness/glibc/exp2.c
+++ correctness/glibc/exp2.c
@@ -1,12 +1,13 @@
 #define __ELEM__ exp2
 #define __MPFR_ELEM__ mpfr_exp2
-#include "LibTestHelper.h"
+#include "LibTestHelperSpecialCases.h"
 
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf("Usage: %s <log file>\n", argv[0]);
         exit(0);
     }
-    RunTest(argv[1], "glibc double exp2");
+    unsigned special_cases[] = {0x3b429d37, 0xbcf3a937};
+    RunTest(argv[1], "glibc double exp2", special_cases);
     return 0;
 }
