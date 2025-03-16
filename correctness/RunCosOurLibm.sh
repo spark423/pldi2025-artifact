a07d7613b27287ab7dd5ef4239fadbc0fb54349b
Sehyeok Park <sp2044@skylake.cs.rutgers.edu>
2025-03-15 20:27:21 -0400

    correcting directory name in ourlibm script for cos

--- correctness/RunCosOurLibm.sh
+++ correctness/RunCosOurLibm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-touch results/ourlibm/ourlibm_ri_input_bounds_cosf.txt
-touch results/ourlibm/ourlibm_ri_outputs_cosf.txt
+touch results/ourlibm/ri_input_bounds_cosf.txt
+touch results/ourlibm/ri_outputs_cosf.txt
 echo -e "|  cos(x)  |"
-./ourlibm/cosf results/ourlibm/ourlibm_ri_input_bounds_cosf.txt
-./ourlibm/cosf_rz results/ourlibm/ourlibm_ri_outputs_cosf_rz.txt
+./ourlibm/cosf results/ourlibm/ri_input_bounds_cosf.txt
+./ourlibm/cosf_rz results/ourlibm/ri_outputs_cosf_rz.txt
