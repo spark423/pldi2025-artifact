a3267b3eaeaedefc60266d26a3f8bbe210586bc5
Sehyeok Park <sp2044@sunny-cove.cs.rutgers.edu>
2025-03-17 16:59:11 -0400

    Fixing script for log10

--- full-gen/Log10-RI-Outputs-Poly.sh
+++ full-gen/Log10-RI-Outputs-Poly.sh
@@ -11,6 +11,12 @@ echo -e "Polynomial generation complete."
 echo -e "Generating the 2nd polynomial for log10."
 ./Log10-RI-Outputs ../intervals/log10_ri_outputs_p2_uq ../polynomials/log10_ri_outputs_p2 20
 echo -e "Polynomial generation complete."
+echo -e "Generating the 3rd polynomial for log10."
+./Log10-RI-Outputs ../intervals/log10_ri_outputs_p3_uq ../polynomials/log10_ri_outputs_p3 20
+echo -e "Polynomial generation complete."
+echo -e "Generating the 4th polynomial for log10."
+./Log10-RI-Outputs ../intervals/log10_ri_outputs_p4_uq ../polynomials/log10_ri_outputs_p4 20
+echo -e "Polynomial generation complete."
 echo -e "Finished generating all polynomials for log10."
 
 
