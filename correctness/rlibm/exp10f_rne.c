3b79c9410f067cff74d6d08a5dccb2321e0bddb9
Sehyeok Park <sp2044@sunny-cove.cs.rutgers.edu>
2025-03-20 19:49:01 -0400

    Correcting test for our exp10f under RNE

--- correctness/rlibm/exp10f_rne.c
+++ correctness/rlibm/exp10f_rne.c
@@ -1,6 +1,6 @@
 #define __ELEM__ rlibm_exp10f
 #define __MPFR_ELEM__ mpfr_exp10
-#include "LibTestHelperSpecialCases.h"
+#include "LibTestHelperSpecialCasesRNE.h"
 
 int main(int argc, char** argv) {
     unsigned special_cases[] = {0x3d781ef5, 0xba6597d7, 0xbd3a8dd9};
